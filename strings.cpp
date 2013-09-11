4b77154560527cdce1848c1a4ba302743b2dc4b1
alyssaq <alyssaquek@gmail.com>
2013-09-11 23:04:46 +0800

    horner modular hash function

--- strings.cpp
+++ strings.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <algorithm> 
+#include <sstream> //stringstream
 
 using namespace std;
 
@@ -11,9 +12,19 @@ int hash(string &str, int tableLen) {
   int sum;
   for (int i = 0; i < str.length(); ++i) {
     cout << str[i] << " " << (int)str[i] << "\n";
-    sum = sum + (int)(str[i]);
+    sum = sum + (int)(str[i]) - (int)'a';
   }
   return sum % tableLen;
+}
+
+const int BASE = 10;
+const int HUGE_NUM = 977;
+int horner_hash(string &str) {
+  int h = 0;
+  for (int i = 0; i < str.length(); ++i) {
+    h = (h * BASE + (int)str[i]) % HUGE_NUM;
+  }
+  return h;
 }
 
 int main() {
@@ -24,9 +35,24 @@ int main() {
   std::vector<int> third (second.begin(),second.end());  // iterating through second
   std::vector<int> fourth (third);                       // a copy of third
   const int TABLE_LEN = 20;
-  vector<int> v(TABLE_LEN);
+  vector<string> v(TABLE_LEN, "!");
   string input;
   cin >> input;
-  v.push_back(hash(input, TABLE_LEN));
-  copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+  v[hash(input, TABLE_LEN)] = input;
+  copy(v.begin(), v.end(), ostream_iterator<string>(cout, " "));
+
+  cout << "\n";
+
+  int i = 0;
+
+  vector<int> hashNums;
+  while (i < 5) {
+    cin >> input;
+    hashNums.push_back(horner_hash(input));
+    i++;
+  }
+
+  //copy(strs.begin(), strs.end(), ostream_iterator<string>(cout, " "));
+  //transform(strs.begin(), strs.end(), hashNums.begin(), horner_hash);
+  copy(hashNums.begin(), hashNums.end(), ostream_iterator<int>(cout, " "));
 }
